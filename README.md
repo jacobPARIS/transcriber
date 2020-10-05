ecd2b0a569f2fcecca273ec8db17b0f30e5f46df
Jacob Paris <jacob@jacobpariseau.com>
2020-10-05 06:51:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,27 +1,6 @@
-# react-boilerplate
-A simple boilerplate for bootstrapping a new React application
+# transcriber
+A tool for generating markdown notes from Discord conversations
 
-# Usage
-
-Create a new respository based on this as a template
-
-Clone that repository to your local environment
-
-```sh
-git clone https://github.com/jacobparis/react-boilerplate my-project
-```
-
-Install node modules
-
-```sh
-npm install
-```
-
-Register the repository with Netlify
-
-Link
 # Commands
 
-* `npm run build` to compile and drop the bundle in the `dist` directory
-
-* `npm run serve` to start a local webserver
+* `npm run serve` to start a netlify-dev
--- app/scenes/home.js
+++ app/scenes/home.js
@@ -1,12 +1,123 @@
 import * as React from 'react'
-
-import { useMachine } from '@xstate/react'
-import { AppMachine } from '../machines/AppMachine'
-
 export default function HomeScene() {
-  const [state, send] = useMachine(AppMachine.withContext({}), {
-    devTools: true,
-  })
+  const [input, setInput] = React.useState('')
+  const [includedUsers, setIncludedUsers] = React.useState(new Set())
+  const [unquotedUsers, setUnquotedUsers] = React.useState(new Set())
 
-  return <h1> Home Scene </h1>
+  const output = input
+    .split(/\n(?=.*(?:[AP]M|\d\d\/\d\d\/2020)\n)/g)
+    .map((i) => (console.log(i), i))
+    .filter(Boolean)
+    .map((message) => {
+      const [heading, ...lines] = message.trim().split('\n')
+
+      const [author] = heading.includes('Today')
+        ? heading.split('Today')
+        : heading.includes('Yesterday')
+        ? heading.split('Yesterday')
+        : heading.split(/\d\d\/\d\d\/2020/)
+
+      return {
+        author,
+        lines,
+      }
+    })
+    .filter((message) => message.author)
+
+  const authors = Array.from(
+    output.reduce((authors, message) => {
+      return authors.add(message.author)
+    }, new Set())
+  )
+
+  return (
+    <div className="flex flex-col p-3 column">
+      <div className="align-self-stretch">
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="w-full border rounded"
+        />
+      </div>
+      <div>
+        <strong> Include these users </strong>
+        {authors.map((author) => {
+          return (
+            <div key={author}>
+              <input
+                type="checkbox"
+                id={`include-user-${author}`}
+                checked={includedUsers.has(author)}
+                onChange={() =>
+                  setIncludedUsers((includedUsers) => {
+                    if (includedUsers.has(author)) {
+                      includedUsers.delete(author)
+                    } else {
+                      includedUsers.add(author)
+                    }
+
+                    return new Set(includedUsers)
+                  })
+                }
+              />
+              <label htmlFor={`include-user-${author}`}> {author} </label>
+            </div>
+          )
+        })}
+      </div>
+      <br />
+      <div>
+        <strong> Unquote these users </strong>
+        {authors
+          .filter((author) => includedUsers.has(author))
+          .map((author) => {
+            return (
+              <div key={author}>
+                <input
+                  type="checkbox"
+                  id={`unquote-user-${author}`}
+                  checked={unquotedUsers.has(author)}
+                  onChange={() =>
+                    setUnquotedUsers((unquotedUsers) => {
+                      if (unquotedUsers.has(author)) {
+                        unquotedUsers.delete(author)
+                      } else {
+                        unquotedUsers.add(author)
+                      }
+
+                      return new Set(unquotedUsers)
+                    })
+                  }
+                />
+                <label htmlFor={`unquote-user-${author}`}> {author} </label>
+              </div>
+            )
+          })}
+      </div>
+      <br />
+      <strong> Transcript </strong>
+      <div className="align-self-stretch">
+        <pre className="whitespace-pre-wrap">
+          {output
+            .filter((message) => includedUsers.has(message.author))
+            .map((message) => {
+              return (
+                <div key={message.lines} className="pb-3">
+                  {unquotedUsers.has(message.author) ? '' : message.author}
+                  <br />
+                  <div>
+                    {message.lines.map((line) => (
+                      <div key={line}>
+                        {unquotedUsers.has(message.author) ? '' : '> '}
+                        {line}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )
+            })}
+        </pre>
+      </div>
+    </div>
+  )
 }
